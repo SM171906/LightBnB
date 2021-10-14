ca67fab89fb5a77eb3e8805ce631eb966833ddfc
Smiti <mishra.smiti06@gmail.com>
2021-10-14 18:10:58 -0400

    update the reservationsa

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -70,16 +70,18 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return pool.query (`SELECT properties.*, reservations.*, avg(rating) as average_rating
+  const queryString = `SELECT properties.*, reservations.*, avg(rating) as average_rating
   FROM reservations
   JOIN properties ON reservations.property_id = properties.id
   JOIN property_reviews ON properties.id = property_reviews.property_id
-  WHERE reservations.guest_id = 1
+  WHERE reservations.guest_id = $1
   AND reservations.end_date < now()::date
   GROUP BY properties.id, reservations.id
-  ORDER BY reservations.start_date guest_id $1
-  LIMIT $2`)
-  .then(res => console.log(res.rows)) //test
+  ORDER BY reservations.start_date 
+   LIMIT $2`
+  return pool.query (queryString, [guest_id, limit])
+  .then(res => {return (res.rows)}) 
+
   .catch(err => console.log(err));
 };
 exports.getAllReservations = getAllReservations;
@@ -94,7 +96,7 @@ exports.getAllReservations = getAllReservations;
  */
  const getAllProperties = function(options, limit = 10) {
   return pool.query(`SELECT * FROM properties LIMIT $1`, [limit])
-  .then(res => console.log(res.rows)) //test
+  .then(res => {return(res.rows)}) //test
   .catch(err => console.log(err));
 };
 exports.getAllProperties = getAllProperties;
