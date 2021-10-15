198dac2506e1303ae639137c0c1761c019ac72d0
Smiti <mishra.smiti06@gmail.com>
2021-10-15 13:10:22 -0400

    add all 3 challenges

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -95,9 +95,46 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
  const getAllProperties = function(options, limit = 10) {
-  return pool.query(`SELECT * FROM properties LIMIT $1`, [limit])
-  .then(res => {return(res.rows)}) //test
-  .catch(err => console.log(err));
+  
+  const queryParams = [];
+  // 2
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
+
+  // 3
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length} `;
+  }
+  if(options.owner_id){
+    queryParams.push(`%${options.owner_id}%`);
+    
+    if(options.city){
+      queryString += `AND `;
+    } else {
+      queryString += `WHERE `;
+    }
+    queryString += `properties.owner_id = $${queryParams.length}`;
+  }
+  if (minimum_rating) {
+    queryParams.push(minimum_rating);
+    queryString += `HAVING avg(property_reviews.rating) >= $${queryParams.length} `;
+  }
+  queryParams.push(limit);
+  queryString += `
+  GROUP BY properties.id
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  // 5
+  console.log(queryString, queryParams);
+
+  // 6
+  return pool.query(queryString, queryParams).then((res) => res.rows);
 };
 exports.getAllProperties = getAllProperties;
 
